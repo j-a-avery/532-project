45067964a62772323b10585d592ca6ac26947ca4
James Avery <j-a-avery@users.noreply.github.com>
2018-03-28 17:49:37 -0500

    Minor change to README.md. Renamed /project to /src. Emptied main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * 
+ * James A. Avery
+ * 50189091
+ * CSCI 532 Semester Project
+ * 
+ * Problem #3 (expanded)
+ * Write a C/C++/Matlab/Java program to compute insertion sort and merge sort.
+ * (You can use the code from the textbooks by Deitel and Deitel or from the
+ *  web.) 
+ * Obtain the run time of both routines. 
+ * The input data should be an int array containing random element values 
+ *  (between, say, 0 and 1023). 
+ * Obtain run time T with 1D (input) array size of n=16, 256, 4096, 65536, 
+ *  1048576 (which equal 2^p, where p = 4, 8, 12, 16, 20).
+ * The run time for each n should be average with about floor(512/(p*p)) runs.
+ * Each run (for a given n) should use a different random input.
+ * Plot (with Excel, Matlab, or other available tools) the run time for both
+ *  routines on one plot, with the x axis in p values, and y axis in log(T).
+ * Label on the plot which curve is for insertion sort and which is for merge
+ *  sort.
+ * Submit C/C++ programs and plot, with instructions in readme.txt on how to 
+ *  build and run the program. (Include the Dev-C++ or MS Studio or Java
+ *  NetBeans project file.)
+ * 
+ *****************************************************************************/
+
+/******************************************************************************
+ * References
+ * Timing code in C++11: 
+ *      https://solarianprogrammer.com/2012/10/14/cpp-11-timing-code-performance/
+ * Time durations, and "using":
+ *      http://en.cppreference.com/w/cpp/chrono/duration
+ * Random numbers in C++11:
+ *      http://en.cppreference.com/w/cpp/numeric/random
+ * ***************************************************************************/
+
+
+int main()
+{
+    
+
+    return 0;
+}
