319ed66b0e48fb081c369ebc1f02fcae2ceef214
James Avery <j-a-avery@users.noreply.github.com>
2018-04-09 21:11:55 -0500

    Removed parallel C++ and serial Go versions. Beautified code.

--- go/532-project-parallel/project.go
+++ go/532-project-parallel/project.go
@@ -1,6 +1,40 @@
-package main
+/******************************************************************************
+ * 
+ * James A. Avery
+ * 50189091
+ * CSCI 532 Semester Project
+ * 
+ * Problem #3 (expanded)
+ * Write a C/C++/Matlab/Java program to compute insertion sort and merge sort.
+ * (You can use the code from the textbooks by Deitel and Deitel or from the
+ *  web.) 
+ * Obtain the run time of both routines. 
+ * The input data should be an int array containing random element values 
+ *  (between, say, 0 and 1023). 
+ * Obtain run time T with 1D (input) array size of n=16, 256, 4096, 65536, 
+ *  1048576 (which equal 2^p, where p = 4, 8, 12, 16, 20).
+ * The run time for each n should be average with about floor(512/(p*p)) runs.
+ * Each run (for a given n) should use a different random input.
+ * Plot (with Excel, Matlab, or other available tools) the run time for both
+ *  routines on one plot, with the x axis in p values, and y axis in log(T).
+ * Label on the plot which curve is for insertion sort and which is for merge
+ *  sort.
+ * Submit C/C++ programs and plot, with instructions in readme.txt on how to 
+ *  build and run the program. (Include the Dev-C++ or MS Studio or Java
+ *  NetBeans project file.)
+ * 
+ *****************************************************************************/
 
-// From https://austingwalters.com/merge-sort-in-go-golang/
+/**  References  **************************************************************
+ * 
+ *  Tour of Go
+ *		https://tour.golang.org/
+ *	Merge Sort in Go
+ * 		https://austingwalters.com/merge-sort-in-go-golang/
+ * 
+******************************************************************************/
+
+package main
 
 import (
 	"fmt"
@@ -16,30 +50,117 @@ import (
 )
 
 
+/* 
+By default, the program runs a decreasing number of trials, such that for an
+    array of size n=2^p, the number of trials is floor(512/(p*p)), (per the
+    project instructions).
+I would like a more scientific approach (i.e. a more) uniform number of 
+    trials), so I have included a command line option to run the same number of
+    trials for each vector size. On my computer, a slice of n = 2^20 = 1048567
+	can be sorted with insertion sort in around four minutes, and the entire
+	project, with 24-element slices, can run in under an hour.
+*/
+const StandardBatchSize int = 24
 
-var StandardBatchSize int = 24
+/*
+If, instead, the program is being run as assigned, then calculate the batch
+	size based on the formula floor(512/p^2)
+This function is defined inline in the C++ version; my understanding is that Go
+	lacks an "inline" keyword, but the Go compiler would likely build this as
+	an inline function.
+*/
+func assignedBatchSize( p int ) int {
+	return int( math.Floor( 512 / ( float64(p) * float64(p) ) ) )
+}
+
+
+/*
+Arrays of size n = 20 take several minutes to run. Limiting the length of
+	arrays to those that can be sorted quickly speeds testing of other issues.
+*/
 const MaxN int = 1000000
+
+/*
+A global WaitGroup to hold all the various tests
+*/
 var wg sync.WaitGroup
 
 
+
+// Struct to allow passing a function along with its name
 type NamedFunction struct {
 	name string
 	function func([]int) []int
 }
 
 
-func TimeRun(f func([]int) []int, c chan int64, v []int) {
-	defer wg.Done()
-	start := time.Now()
-	f(v)
-	duration := time.Now().Sub(start)
-	fmt.Printf("%v ", duration)	
-	c <- duration.Nanoseconds()
+
+func main() {
+	// Put the command line arguments into an array for easier access
+	args := os.Args
+
+	// Flags to determine run-time behavior
+	// Default values are the problem as assigned
+	// Changing these through command line options results in shorter or more interesting tests
+	runInsertionSort := true
+	runMergeSort := true
+	limitN := false
+	demo := false
+	time := true;
+
+    // p determines both the array size and the number of tests per array
+    // Arrays of size 2^p will be tested either floor(512/(p*p)) times or 32 times
+	// Having a vector of ps will make things easier later
+	ps := []int{ 4, 8, 12, 16, 20 }
+
+	// Determine the sizes of the test runs
+	// If false, then each vector of size 2^p will be tested floor(152/(p*p)) times
+	// If true, each vector will be tested the same number of times (set above)
+	equalBatchSizes := false
+
+
+	// Adjust run-time flags according to command line arguments
+	for _, flag := range args {
+		if flag == "-insertion" { runInsertionSort = false; }
+		if flag == "-merge" { runMergeSort = false; }
+		if flag == "equal-batches" { equalBatchSizes = true; }
+		if flag == "limit-n" { limitN = true; }
+		if flag == "demo" { demo = true; }
+		if flag == "-time" { time = false; }
+	}
+
+	// Create a slice of functions to test, based on command line options
+	//	Default is both Insertion Sort and Merge Sort,
+	//	but these can be stopped by command line options above
+	var sorters []NamedFunction
+	if runInsertionSort {
+		sorters = append(sorters, NamedFunction{"Insertion Sort", InsertionSort})
+	}
+	if runMergeSort {
+		sorters = append(sorters, NamedFunction{"Merge Sort", MergeSort})
+	}
+
+	// Demonstrate that the sort functions sort correctly (disabled by default)
+	if demo {
+		for _, sorter := range sorters {
+			demoSorter(sorter)
+		}
+	}
+
+	// The heart of the project
+	// Time the run-time of each sort function as assigned (enabled by default)
+	if time {
+		for _, sorter := range sorters {
+			testSorter(sorter, ps, equalBatchSizes, limitN)
+		}
+	}
+
+	return
 }
 
 
 
-// InsertionSort - runs insertion sort algorithm on a slice
+// Sort a slice in place using insertion sort and return the slice
 func InsertionSort(v []int) []int {
 	// Loop through the elements of the array
 	for j := 1; j < len(v); j++ {
@@ -57,7 +178,10 @@ func InsertionSort(v []int) []int {
 	return v
 }
 
-// MergeSort - runs MergeSort algorithm on a single slice
+
+
+// Sort a slice in place using Merge Sort, and return the slice
+//	From https://austingwalters.com/merge-sort-in-go-golang/
 func MergeSort(slice []int) []int {
 	if len(slice) < 2 {
 		return slice;
@@ -66,6 +190,8 @@ func MergeSort(slice []int) []int {
 	mid := (len(slice)) / 2
 	return Merge(MergeSort(slice[ : mid]), MergeSort(slice[mid : ]))
 }
+
+
 
 // Merge - merges left and right slices into newly created slice
 func Merge(left, right []int) []int {
@@ -95,6 +221,7 @@ func Merge(left, right []int) []int {
 
 
 
+// Fill a vector in place with random integers
 func RandomFill(v []int) {
 	for i := 0; i < len(v); i++ {
 		v[i] = rand.Intn(1024)
@@ -103,6 +230,11 @@ func RandomFill(v []int) {
 
 
 
+/* Given a NamedFunction, a slice of values for p, and booleans to determine
+	whether or not to process equal batch sizes and whether or not to limit
+	the size of the slice to slices that can be sorted quickly, calculate the
+	execution time for each of the sort algorithms.
+*/
 func testSorter(f NamedFunction, ps []int, equalBatchSizes, limitN bool) {
 	// Create a file to output the results
 	fileName := strings.ToLower(strings.Map(
@@ -123,21 +255,20 @@ func testSorter(f NamedFunction, ps []int, equalBatchSizes, limitN bool) {
 	writer := csv.NewWriter(file)
 	fmt.Println("\n", f.name)
 
+	// Iterate through each of the p values (p = 4, 8, 12, ...)
 	for _, p := range ps {
+		// Vector size
 		n := int(math.Pow(2, float64(p)))
 
-		if limitN && n > MaxN {
-			continue
-		}
+        // If n is being limited to certain fast-running small values,
+        //  then skip this iteration
+		if limitN && n > MaxN { continue }
 
-		var batchSize int
+		// Calculate batch size
+		batchSize := StandardBatchSize
+		if equalBatchSizes { batchSize = assignedBatchSize( p ) }
 
-		if equalBatchSizes {
-			batchSize = StandardBatchSize
-		} else {
-			batchSize = int(math.Floor(512 / (float64(p)*float64(p))))
-		}
-
+		// First column of the output is the size of the slice
 		fmt.Printf("\n%v:\n", n)
 
 		var results []string
@@ -172,87 +303,20 @@ func testSorter(f NamedFunction, ps []int, equalBatchSizes, limitN bool) {
 
 
 
-
-func main() {
-	// Put the command line arguments into a vector for easier access
-	args := os.Args
-
-	// Flags to determine run-time behavior
-	// Default values are the problem as assigned
-	// Changing these through command line options results in shorter or more interesting tests
-	runInsertionSort := true
-	runMergeSort := true
-	limitN := false
-	demo := false
-	time := true;
-
-    // p determines both the array size and the number of tests per array
-    // Arrays of size 2^p will be tested either floor(512/(p*p)) times or 32 times
-	// Having a vector of ps will make things easier later
-	ps := []int{ 4, 8, 12, 16, 20 }
-
-	// Determine the sizes of the test runs
-	// If false, then each vector of size 2^p will be tested floor(152/(p*p)) times
-	// If true, each vector will be tested the same number of times (set above)
-	equalBatchSizes := false
-
-	// Adjust run-time flags according to command line arguments
-	for _, flag := range args {
-		if flag == "-insertion" { runInsertionSort = false; }
-		if flag == "-merge" { runMergeSort = false; }
-		if flag == "equal-batches" { equalBatchSizes = true; }
-		if flag == "limit-n" { limitN = true; }
-		if flag == "demo" { demo = true; }
-		if flag == "-time" { time = false; }
-	}
-
-	// Create a slice of functions to test, based on command line options
-	//	Default is both Insertion Sort and Merge Sort,
-	//	but these can be stopped by command line options above
-	var sorters []NamedFunction
-	if runInsertionSort {
-		sorters = append(sorters, NamedFunction{"Insertion Sort", InsertionSort})
-	}
-	if runMergeSort {
-		sorters = append(sorters, NamedFunction{"Merge Sort", MergeSort})
-	}
-
-	// Demonstrate that the sort functions sort correctly (disabled by default)
-	if demo {
-		for _, sorter := range sorters {
-			demoSorter(sorter)
-		}
-	}
-
-	// Time the sort functions, as assigned (enabled by default)
-	if time {
-		for _, sorter := range sorters {
-			testSorter(sorter, ps, equalBatchSizes, limitN)
-		}
-	}
-
-	return
+// Find the length of time it takes to run a sort,
+//	and push the time (in nanoseconds) to the channel, c
+func TimeRun(f func([]int) []int, c chan int64, v []int) {
+	defer wg.Done()
+	start := time.Now()
+	f(v)
+	duration := time.Now().Sub(start)
+	fmt.Printf("%v ", duration)	
+	c <- duration.Nanoseconds()
 }
 
 
 
-func head(v []int) {
-	var length int
-
-	if (len(v) > 10) {
-		length = 10
-	} else {
-		length = len(v)
-	}
-
-	for i := 0; i < length; i++ {
-		fmt.Printf("%v ", v[i])
-	}
-	fmt.Println()
-}
-
-
-
+// Demonstrate that the sorter actually does sort a slice properly
 func demoSorter(sorter NamedFunction) {
 	fmt.Printf("\n=====  Demonstrating %v  =====\n", sorter.name)
 	for i := 0; i < 10; i++ {
--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,9 @@
  *  Using std::copy instead of a for loop for a single-line vector print:
  *      https://stackoverflow.com/questions/10750057/how-to-print-out-the-contents-of-a-vector/11335634#11335634 
  * 
+ *  Merge Sort from Deitel and Deitel
+ *  Insertion Sort from Deitel and Deitel
+ * 
 ******************************************************************************/
 
 #include <iostream>
@@ -57,11 +60,18 @@ I would like a more scientific approach (i.e. a more) uniform number of
     trials), so I have included a command line option to run the same number of
     trials for each vector size. On my computer, a vector of n = 2^20 = 1048567
     can be sorted in around 24 minutes using insertion sort, so a batch size of
-    16 allows the entire program to run overnight.
-STANDARD_BATCH_SIZE is declared as a variable instead of a constexpr in case
-    I decide to add a command line option to use a custom batch size.
+    24 allows the entire program to run roughly overnight.
 */
-int STANDARD_BATCH_SIZE = 16;
+constexpr int STANDARD_BATCH_SIZE = 24;
+
+/*
+If, instead, the program is being run as assigned, then calculate the batch
+    size based on the formula floor(512/p^2)
+*/
+inline int assigned_batch_size( int p ) {
+    return floor ( 512 / ( p * p ) );
+}
+
 /*
 Arrays of size n = 20 take almost half an hour to run, which is far too long
     for testing. This limits the size of arrays to those that can be processed
@@ -69,42 +79,47 @@ Arrays of size n = 20 take almost half an hour to run, which is far too long
 */
 constexpr int MAX_N = 1000000;
 
+// Struct to allow passing both a function and some name for it
+struct NamedFunction {
+    std::string function_name;
+    std::function<void( std::vector<int>& )> function;
+};
+
+
 
 // Sort a vector in place using insertion sort
-void insertion_sort( std::vector< int > & );
+void insertion_sort( std::vector<int>& );
 
 
 // Sort a vector in place using merge sort
-void merge_sort( std::vector< int > & );
+void merge_sort( std::vector<int>& );
 // Merge sort helper functions
-void sort_sub_vector( std::vector< int > &, int, int );
-void merge( std::vector< int > &, int, int, int, int );
+void merge( std::vector<int> &, int, int, int, int );
+void sort_sub_vector( std::vector<int>&, int, int );
 
 
-
-// Fill an int vector with random ints
-void random_fill( std::vector< int > & );
+// Fill an int vector with random integers
+void random_fill( std::vector<int>& );
 
 
 // Overload operator<< for vectors to simplify output of all vector elements
 template<typename T>
-std::ostream & operator<<( std::ostream &, const std::vector< T > & );
+std::ostream & operator<<( std::ostream&, const std::vector<T>& );
 
-// Struct to allow passing both a function and some name for it
-struct NamedFunction {
-    std::string function_name;
-    std::function< void( std::vector< int > & ) > function;
-};
 
 // Function that allows each different sorting algorithm to be tested and timed
 //  using the same code.
-void test_sorter( NamedFunction &, std::vector< int >, bool, bool );
+void test_sorter( NamedFunction&, std::vector<int>, bool, bool );
+
+// Demonstrate that a given sorter function actually does sort a vector properly
+void demo_sorter( NamedFunction );
+
 
 
 
 int main(int argc, char **argv) {
     // Put the command line arguments into a vector for easier access
-    std::vector< std::string > args{argv + 1, argv + argc};
+    std::vector<std::string> args{argv + 1, argv + argc};
 
     // Flags to determine run-time behavior.
     // Default values are the problem as assigned.
@@ -112,11 +127,13 @@ int main(int argc, char **argv) {
     bool run_insertion_sort = true;
     bool run_merge_sort = true;
     bool limit_n = false;
+    bool demo = false;
+    bool time = true;
     
     // p determines both the array size and the number of tests per array
     // Arrays of size 2^p will be tested either floor(512/(p*p)) times or 32 times
     // Having a vector of ps will make things easier later
-    std::vector< int > ps { 4, 8, 12, 16, 20  };
+    std::vector<int> ps { 4, 8, 12, 16, 20  };
 
     // Determine the sizes of the test runs.
     // If false, then each vector of size 2^p will be tested floor(512/(p*p)) times
@@ -130,11 +147,14 @@ int main(int argc, char **argv) {
         if ( flag == "-merge" ) { run_merge_sort = false; }
         if ( flag == "equal-batches" ) { equal_batch_sizes = true; }
         if ( flag == "limit-n" ) { limit_n = true; }
+        if ( flag == "demo" ) { demo = true; }
+        if ( flag == "-time" ) { time = false; }
     }
 
-    // Create and populate a vector of functions to test
-    std::vector< NamedFunction > sorters;
-
+    // Create a vector of functions to test based on command line options
+    //  Default is both Insertion Sort and Merge Sort,
+    //  but these can be stopped by command line options above
+    std::vector<NamedFunction> sorters;
     if ( run_insertion_sort ) { 
         sorters.push_back( NamedFunction{ "Insertion Sort",  insertion_sort } ); 
     }
@@ -142,77 +162,23 @@ int main(int argc, char **argv) {
         sorters.push_back( NamedFunction{ "Merge Sort", merge_sort } ); 
     }
 
+    // Demonstrate that the sort functions sort correctly (disabled by default)
+    if ( demo ) {
+        for ( auto sorter : sorters ) {
+            demo_sorter ( sorter );
+        }
+    }
+
     // The heart of the project
-    // Test the run-times of each sort function
-    for ( auto sorter : sorters ) {
-        test_sorter( sorter, ps, equal_batch_sizes, limit_n );
+    // Test the run-time of each sort function as assigned (enabled by default)
+    if ( time ) {
+        for ( auto sorter : sorters ) {
+            test_sorter( sorter, ps, equal_batch_sizes, limit_n );
+        }
     }
 
     return 0;
-} // end main
-
-
-
-// Given a NamedFunction (a pointer to a function and a string giving it a name),
-//  a boolean to determine whether or not to process equal batch sizes, and a vector
-//  of values for p, calculate the execution time for each of the sort algorithms.
-void test_sorter( NamedFunction &f, std::vector< int > ps, bool equal_batch_sizes, bool limit_n ) {
-    // Create a file to output the results
-    std::string file_name{ f.function_name };
-    file_name.erase( std::remove_if( file_name.begin(), file_name.end(), isspace ) );
-    std::transform( file_name.begin(), file_name.end(), file_name.begin(), tolower );
-    file_name += ".txt";
-
-    std::ofstream fout(file_name);
-    
-    std::cout << std::endl << f.function_name << std::endl;
-
-    // Iterate through each of the p values (i.e. p = 4, 8, 12, ...)
-    for ( auto p : ps) {
-        // Vector size
-        int n = pow( 2, p );
-
-        // If n is being limited to certain fast-running small values,
-        //  then skip this iteration
-        if ( limit_n && n > MAX_N ) { continue; }
-
-        // Calculate batch size
-        int batch_size;
-        
-        if ( equal_batch_sizes ) {  // Uniform batch sizes for better analysis
-            batch_size = STANDARD_BATCH_SIZE;
-        } else {                    // As assigned for faster run time
-            batch_size = floor( 512 / ( p * p ) );
-        }
-
-        // First column of the output is the size of the array
-        std::cout << n << "\t" << std::flush;
-        fout << n << "\t" << std::flush;
-
-        for ( int trial = 0; trial < batch_size; trial++ ) {
-            // Create a vector and fill it with random numbers
-            std::vector< int > v( n );
-            random_fill( v );
-
-            // Time execution of the sort
-            auto start = std::chrono::steady_clock::now();
-            f.function( v );
-            auto end = std::chrono::steady_clock::now();
-
-            std::cout
-                << std::chrono::duration< unsigned long long, std::nano >(end - start).count()
-                << "\t" << std::flush;
-            fout
-                << std::chrono::duration< unsigned long long, std::nano >(end - start).count()
-                << "\t" << std::flush;
-        }
-
-        std::cout << std::endl;
-        fout << std::endl;
-    }
-
-    fout.close();
-} // end test_sorter
+}
 
 
 
@@ -221,26 +187,25 @@ void insertion_sort( std::vector<int> &v ) {
     // Loop throught the elements of the array
     for ( int j = 1; j < v.size(); j++ )
     {
-        int key = v[ j ];   // 
-
+        int key = v[j];
         int i = j - 1;
         
         while ( i >= 0 && v[i] > key )
         {
-            v[i + 1] = v[i];
+            v[ i + 1 ] = v[i];
             i -= 1;
         }
 
         v[ i + 1 ] = key;
     }
-} // end insertion_sort
+}
 
 
 
 // Sort a vector in place using merge sort
 void merge_sort( std::vector<int> &v ) {
     sort_sub_vector( v, 0, v.size() - 1 );
-} // end merge_sort
+}
 
 
 
@@ -258,7 +223,7 @@ void merge( std::vector<int> &v, int left, int mid1, int mid2, int right ) {
         if ( v[ left_index ] <= v[ right_index ] ) {
             combined[ combined_index++ ] = v[ left_index++ ];
         } else {
-            combined [ combined_index++ ] = v[ right_index++ ];
+            combined[ combined_index++ ] = v[ right_index++ ];
         }
     }
 
@@ -275,9 +240,9 @@ void merge( std::vector<int> &v, int left, int mid1, int mid2, int right ) {
 
     // Copy values back into the original vector
     for ( int i = left; i <= right; i++ ) {
-        v[ i ] = combined [ i ];
+        v[i] = combined[i];
     }
-} // end merge
+}
 
 
 
@@ -298,27 +263,107 @@ void sort_sub_vector( std::vector<int> &v, int low, int high ) {
         // Merge the two sorted vectors
         merge( v, low, mid1, mid2, high );
     } // end if not base case
-} // end sort_sub_vector
+}
 
 
 
-// Fill a vector with random integers
+// Fill a vector in place with random integers
 void random_fill( std::vector<int> &v ) {
+    // Initialize the C++11 random device
     static std::random_device rd{};
-    static std::mt19937 mt{rd()};
-    static std::uniform_int_distribution<int> dist{1, 1024};
+    static std::mt19937 mt{ rd() };
+    static std::uniform_int_distribution<int> dist{ 1, 1024 };
 
     for ( int i = 0; i < v.size(); i++ ) 
     {
-        v[i] = dist(mt);
+        v[i] = dist( mt );
     }
-} // end random_fill
+}
 
 
 
 // Overload operator<< for vectors to simplify output of all vector elements
 template <typename T>
-std::ostream& operator<< ( std::ostream &out, const std::vector< T > &v ) {
-    std::copy( v.begin(), v.end(), std::ostream_iterator< T >( out, " ") );
+std::ostream& operator<< ( std::ostream &out, const std::vector<T> &v ) {
+    std::copy( v.begin(), v.end(), std::ostream_iterator<T>( out, " " ) );
     return out;
-} // end operator<<
+}
+
+
+
+// Given a NamedFunction, a vector of values for p, and booleans to determine
+//  whether or not to process equal batch sizes and whether or not to limit
+//  the size of the vector to vectors that can be sorted quickly, calculate the
+//  execution times for each of the sort algorithms
+void test_sorter( NamedFunction &f, std::vector<int> ps, 
+                    bool equal_batch_sizes, 
+                    bool limit_n ) {
+    // Create a file to output the results
+    std::string file_name{ f.function_name };
+    file_name.erase( std::remove_if( file_name.begin(), file_name.end(), isspace ) );
+    std::transform( file_name.begin(), file_name.end(), file_name.begin(), tolower );
+    file_name += ".csv";
+
+    std::ofstream fout( file_name );
+    
+    std::cout << std::endl << f.function_name << std::endl;
+
+    // Iterate through each of the p values (i.e. p = 4, 8, 12, ...)
+    for ( int p : ps ) {
+        // Vector size
+        int n = pow( 2, p );
+
+        // If n is being limited to certain fast-running small values,
+        //  then skip this iteration
+        if ( limit_n && n > MAX_N ) { continue; }
+
+        // Calculate batch size
+        int batch_size = equal_batch_sizes ? 
+                         STANDARD_BATCH_SIZE : 
+                         assigned_batch_size( p );
+
+        // First column of the output is the size of the vector
+        std::cout << n << "\t" << std::flush;
+        fout << n << "\t" << std::flush;
+
+        for ( int trial = 0; trial < batch_size; trial++ ) {
+            // Create a vector and fill it with random numbers
+            std::vector<int> v( n );
+            random_fill( v );
+
+            // Time execution of the sort
+            auto start = std::chrono::steady_clock::now();
+            f.function( v );
+            auto end = std::chrono::steady_clock::now();
+
+            std::cout
+                << std::chrono::duration<unsigned long long, std::nano>(end - start).count()
+                << "\t" << std::flush;
+            fout
+                << std::chrono::duration<unsigned long long, std::nano>(end - start).count()
+                << "\t" << std::flush;
+        }
+
+        std::cout << std::endl;
+        fout << std::endl;
+    }
+
+    fout.close();
+}
+
+
+
+// Demonstrate that the sorter actually does sort a vector properly
+void demo_sorter( NamedFunction sorter ) {
+    std::cout << std::endl << "=====  Demonstrating " 
+            << sorter.function_name
+            << "  =====" << std::endl;
+
+    for ( int i = 0; i < 10; i++ ) {
+        std::vector<int> v( 10 );
+        random_fill( v );
+        std::cout << std::endl << "Before: " << v << std::endl;
+        sorter.function(v);
+        std::cout << "After: " << v << std::endl;
+    }
+}
